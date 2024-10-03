919c46be26912a05a5b22fffee4020dabc78e660
mansuroff123 <mansuroff145@gmail.com>
2024-10-03 17:25:52 +0500

    components style

---
+++ src/components/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function App() {
+    return (
+        <div>
+            <h1 className="heading">My Contacts</h1>
+            <div className="card">
+                <div className="top">
+                    <h2 className="name">Name...</h2>
+                    <img src="" alt="profile"/>
+                </div>
+                <div className="bottom">
+                    <p className="info">Phone number</p>
+                    <p className="info">Email</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default App;
---
+++ src/contacts.js
@@ -0,0 +1,20 @@
+const contacts = [
+    {
+        name: "Damian Jefferson",
+        imgURL: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+        phone: "+123 657 231",
+        email: "Damian@gmail.com"
+    }, 
+    {
+        name: "Leonardo Bernard",
+        imgURL: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+        phone: "+543 234 327",
+        email: "Leonardo@gmail.com"
+    },
+    {
+        name: "Keenan Finley",
+        imgURL: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+        phone: "+834 125 063",
+        email: "Keenan@gmail.com"
+    }
+]
